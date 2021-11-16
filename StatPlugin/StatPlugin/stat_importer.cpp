4fcdc24e3f835c51a702c134674136b796b28cdf
roleary11 <ryanoleary106@gmail.com>
2021-11-16 17:41:23 -0600

    more starter files

---
+++ StatPlugin/StatPlugin/stat_importer.cpp
@@ -0,0 +1,7 @@
+#include "stat_importer.h"
+
+using namespace statimporter;
+
+void STAT_IMPORTER::import_file(String fileName) {
+
+}
---
+++ StatPlugin/StatPlugin/stat_importer.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef STAT_IMPORTER_H
+#define STAT_IMPORTER_H
+
+namespace statimporter
+{
+    class stat_importer {
+    private:    
+        
+
+    public:
+        void import_file(String fileName);
+    
+    };
+}
+
+#endif
