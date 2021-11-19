fc4b987dfe471add8af6fac3066cb605e6b81772
roleary11 <ryanoleary106@gmail.com>
2021-11-19 17:43:38 -0600

    statplugin and dll

--- StatPlugin/StatPlugin/stat_importer.cpp
+++ StatPlugin/StatPlugin/stat_importer.cpp
@@ -1,92 +1,92 @@
 #include "stat_importer.h"
 
-using namespace statimporter;
 using namespace std;
 
+extern "C" {
+	void read_stats(map<string, int> stats, map<std::string, map<std::string, vector<int>>> attacks_list, std::string fileName) {
+		ifstream inFile;
 
-void STAT_IMPORTER::read_stats(string fileName) {
-	inFile.open(fileName);
+		// map<string, int> stats;
+        // map<std::string, map<std::string, vector<int>>> attacks_list;
+		
+		inFile.open(fileName);
 
-	if (!inFile) {
-		cerr << ("Unable to open file " + fileName);
-		return;
-	}
+		if (!inFile) {
+			cerr << ("Unable to open file " + fileName);
+			return;
+		}
 
-	string nextLine;
-	while (getline(inFile, nextLine)) {
-		// read in stats
-		if (nextLine.find(',') != -1) {
-			string delim = ",";
-			vector<string> attacks = split(nextLine, delim);
-			string attack_type = attacks[0];
-			attacks.erase(attacks.begin());
+		string nextLine;
+		while (getline(inFile, nextLine)) {
+			// read in stats
+			if (nextLine.find(',') != -1) {
+				string delim = ",";
+				vector<string> attacks = split(nextLine, delim);
+				string attack_type = attacks[0];
+				attacks.erase(attacks.begin());
 
-			map<string,vector<int>> initmap;
-			// attacks_list.insert(attack_type,initmap);
-			attacks_list[attack_type] = initmap;
-			for(string attack : attacks) {
-				vector<string> values = split(attack,":");
-				vector<int> dmgs;
-				dmgs.push_back(stoi(values[1]));
-				dmgs.push_back(stoi(values[2]));
-				// attacks_list.get(attack_type).insert(values[0],dmgs);
+				map<string, vector<int>> initmap;
+				// attacks_list.insert(attack_type,initmap);
+				attacks_list[attack_type] = initmap;
+				for (string attack : attacks) {
+					vector<string> values = split(attack, ":");
+					vector<int> dmgs;
+					dmgs.push_back(stoi(values[1]));
+					dmgs.push_back(stoi(values[2]));
+					// attacks_list.get(attack_type).insert(values[0],dmgs);
 
-				attacks_list[attack_type][values[0]] = dmgs;
+					attacks_list[attack_type][values[0]] = dmgs;
+				}
+			}
+
+			else {
+				string delim = ":";
+				string field = nextLine.substr(0, nextLine.find(delim));
+				string value = nextLine.substr(nextLine.find(delim) + 1, nextLine.length());
+
+				stats[field] = stoi(value);
 			}
 		}
-		
-		else {
-			string delim = ":";
-			string field = nextLine.substr(0, nextLine.find(delim));
-			string value = nextLine.substr(nextLine.find(delim)+1, nextLine.length());
-
-			stats[field] = stoi(value);
-		}	
+		inFile.close();
 	}
-	inFile.close();
-}
 
-// for string delimiter
-// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
-vector<string> STAT_IMPORTER::split (string s, string delimiter) {
-    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-    string token;
-    vector<string> res;
+	// for string delimiter
+	// https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+	vector<string> split(string s, string delimiter) {
+		size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+		string token;
+		vector<string> res;
 
-    while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
-        token = s.substr(pos_start, pos_end - pos_start);
-        pos_start = pos_end + delim_len;
-        res.push_back(token);
-    }
+		while ((pos_end = s.find(delimiter, pos_start)) != string::npos) {
+			token = s.substr(pos_start, pos_end - pos_start);
+			pos_start = pos_end + delim_len;
+			res.push_back(token);
+		}
 
-    res.push_back (s.substr (pos_start));
-    return res;
-}
-
-
-void STAT_IMPORTER::create_player() {
-
+		res.push_back(s.substr(pos_start));
+		return res;
+	}
 }
 
 
 int main(int argc, char *argv[]) {
-	STAT_IMPORTER stat_imp;
+	// STAT_IMPORTER stat_imp;
 	
-	stat_imp.read_stats("Squirtle.txt");
-	//print stats
-	for (auto it = stat_imp.stats.begin(); it != stat_imp.stats.end(); ++it)
-		std::cout << it->first << " => " << it->second << '\n';
+	// stat_imp.read_stats("Squirtle.txt");
+	// //print stats
+	// for (auto it = stat_imp.stats.begin(); it != stat_imp.stats.end(); ++it)
+	// 	std::cout << it->first << " => " << it->second << '\n';
 
-	//print attack list
-	for (auto it = stat_imp.attacks_list.begin(); it != stat_imp.attacks_list.end(); ++it){
-		std::cout << it->first << " => ";
-		for (auto it2 = it->second.begin(); it2 != it->second.end(); ++it2){
-			std::cout << it2->first << " , "; //<< it2->second << '\n';
-			for(int i : it2->second) {
-				std::cout << i;
-			}
-		}
-	}
+	// //print attack list
+	// for (auto it = stat_imp.attacks_list.begin(); it != stat_imp.attacks_list.end(); ++it){
+	// 	std::cout << it->first << " => ";
+	// 	for (auto it2 = it->second.begin(); it2 != it->second.end(); ++it2){
+	// 		std::cout << it2->first << " , "; //<< it2->second << '\n';
+	// 		for(int i : it2->second) {
+	// 			std::cout << i;
+	// 		}
+	// 	}
+	// }
 			
 	return 0;
 }
--- StatPlugin/StatPlugin/stat_importer.h
+++ StatPlugin/StatPlugin/stat_importer.h
@@ -1,33 +1,15 @@
-#pragma once
-#ifndef STAT_IMPORTER_H
-#define STAT_IMPORTER_H
-
+#define STAT_IMPORTER_H __declspec(dllexport)
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <vector>
 #include <map>
 
 using namespace std;
 
-namespace statimporter
-{
-    class STAT_IMPORTER {
-    private:    
-        ifstream inFile;
-
-    public:
-        // put these as private later
-        map<string, int> stats;
-        map<std::string, map<std::string, vector<int>>> attacks_list;
-
-        void read_stats(string fileName); //reads stats from inFile
-
-        vector<string> split(string s, string delimiter); //splits string on delimiter
-
-        void create_player();   //sends stat information to new player/enemy object
-    
-    };
+extern "C" {
+    STAT_IMPORTER_H void read_stats(map<string, int> stats, 
+        map<std::string, map<std::string, vector<int>>> attacks_list, std::string fileName); //reads stats from inFile
+    STAT_IMPORTER_H vector<std::string> split(std::string s, std::string delimiter); //splits string on delimiter    
 }
-
-#endif
